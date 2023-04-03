a779f9303e832fc70254d0677625b016409889df
Bhargav <bhargaveshdakka@gmail.com>
2023-04-03 20:35:35 +0530

    second try

---
+++ cars.py
@@ -0,0 +1,47 @@
+class audi:
+    def __init__(self,m,p,y):
+        self.model=m
+        self.price=p
+        self.year=y
+    def display(self):
+        print(self.model,self.price,self.year)
+audi=audi("audi",188000,2019)
+audi.display()
+class nissan:
+    def __init__(self,m,p,y):
+        self.model=m
+        self.price=p
+        self.year=y
+    def display(self):
+        print(self.model,self.price,self.year)
+nissan=nissan("nissan",188000,2019)
+nissan.display()
+
+class bmw:
+    def __init__(self,m,p,y):
+        self.model=m
+        self.price=p
+        self.year=y
+    def display(self):
+        print(self.model,self.price,self.year)
+bmw=bmw("bmw",188000,2019)
+bmw.display()
+
+
+
+
+
+    
+
+
+
+
+
+    
+
+
+
+
+
+
+    
